cb025ac6e6d9e8498f945119cc503ca1a00609ed
Declan Ramsay <declandr@gmail.com>
2020-06-14 11:55:34 +0100

    Fix get club by slug

--- src/models/clubs.js
+++ src/models/clubs.js
@@ -7,7 +7,7 @@ export const searchClubs = ({ longitude, latitude }, radius) => db.any(sqlSearch
 });
 
 export const getClubBySlug = async (slug) => {
-  const club = await db.oneOrNone(`SELECT *, SELECT *, ST_AsGeoJSON(location) AS coordinates FROM clubs WHERE slug = ${slug};`);
+  const club = await db.oneOrNone(`SELECT *, ST_AsGeoJSON(location) AS coordinates FROM clubs WHERE slug = '${slug}';`);
 
   const teams = await db.any(sqlReadMany, {
     columns: '*',
