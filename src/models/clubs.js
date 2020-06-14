711e9f1bf4b3c7d27de1241f3d94480348f015cb
Declan Ramsay <declandr@gmail.com>
2020-06-14 11:38:25 +0100

    Make direct query for clubs

--- src/models/clubs.js
+++ src/models/clubs.js
@@ -1,5 +1,5 @@
 import { db } from '../modules/pg';
-import { sqlSearchClubs, sqlReadOne, sqlReadMany } from '../sql';
+import { sqlSearchClubs, sqlReadMany } from '../sql';
 
 export const searchClubs = ({ longitude, latitude }, radius) => db.any(sqlSearchClubs, {
   point: `'POINT(${longitude} ${latitude})'`,
@@ -7,12 +7,7 @@ export const searchClubs = ({ longitude, latitude }, radius) => db.any(sqlSearch
 });
 
 export const getClubBySlug = async (slug) => {
-  const club = await db.oneOrNone(sqlReadOne, {
-    columns: '*, ST_AsGeoJSON(location) AS coordinates',
-    table: 'clubs',
-    key: 'slug',
-    value: slug,
-  });
+  const club = await db.oneOrNone(`SELECT *, SELECT *, ST_AsGeoJSON(location) AS coordinates FROM clubs WHERE slug = ${slug};`);
 
   const teams = await db.any(sqlReadMany, {
     columns: '*',
