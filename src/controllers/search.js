df0ed9a99690768fa11ef391ae0f9134e05369ab
Declan Ramsay <declandr@gmail.com>
2020-07-01 23:26:29 +0100

    Get clubs and events around the right  way 🤦

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -2,13 +2,13 @@ import { searchClubs } from '../models/clubs';
 import { searchEvents } from '../models/events';
 
 export const search = async (location, radius, showTypes, leagues) => {
-  let events = [];
   let clubs = [];
+  let events = [];
 
-  if (!showTypes || showTypes.includes('events')) {
+  if (!showTypes || (Array.isArray(showTypes) && showTypes.includes('clubs')) || showTypes === 'clubs') {
     clubs = await searchClubs(location, radius, leagues);
   }
-  if (!showTypes || showTypes.includes('clubs')) {
+  if (!showTypes || (Array.isArray(showTypes) && showTypes.includes('events')) || showTypes === 'events') {
     events = await searchEvents(location, radius, leagues);
   }
 
