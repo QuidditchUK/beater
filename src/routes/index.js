b2583e70ad4f4a82a6bdfd4a7d461212d6361005
Declan Ramsay <declandr@gmail.com>
2020-07-18 22:51:05 +0100

    Add contact route to route index

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import clubsRoute from './clubs';
 import eventsRoute from './events';
 import searchRoute from './search';
 import productsRoute from './products';
+import contactRoute from './contact';
 import webhooksRoute from './webhooks';
 
 const router = express.Router();
@@ -16,6 +17,7 @@ router.use('/clubs', clubsRoute());
 router.use('/events', eventsRoute());
 router.use('/search', searchRoute());
 router.use('/products', productsRoute());
+router.use('/contact', contactRoute());
 router.use('/webhooks', webhooksRoute());
 
 export default router;
