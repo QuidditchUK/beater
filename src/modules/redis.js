7eb8623642023deb963a4562ef4e5d3dd870b2b0
Declan Ramsay <declandr@gmail.com>
2022-04-20 19:00:43 +0100

    Revert redis to original state

--- src/modules/redis.js
+++ src/modules/redis.js
@@ -2,8 +2,9 @@ import { URL } from 'url';
 import Redis from 'ioredis';
 
 import getLogger from './logger';
+import config from '../config';
 
-const settings = new URL(process.env.REDIS_URL);
+const settings = new URL(config.redis.url);
 
 const port = settings.port || 6379;
 const host = settings.hostname || '127.0.0.1';
@@ -11,7 +12,7 @@ const host = settings.hostname || '127.0.0.1';
 export default (key = '') => {
   const logger = getLogger(`redis-${key}`);
 
-  const client = new Redis(port, host, { no_ready_check: true, rejectUnauthorized: false });
+  const client = new Redis(port, host, { no_ready_check: true });
 
   if (settings.username && settings.password) {
     client.auth(settings.username, settings.password);
