ba6d2316249d3567e140b67a327d934b76c3baf8
Declan Ramsay <declan.ramsay@starlingbank.com>
2022-04-22 10:38:15 +0100

    Tidy OIDC

--- src/modules/redis.js
+++ src/modules/redis.js
@@ -2,21 +2,11 @@
 import Redis from 'ioredis';
 
 import getLogger from './logger';
-// import config from '../config';
-
-// const settings = new URL(config.redis.url);
-
-// const port = settings.port || 6379;
-// const host = settings.hostname || '127.0.0.1';
 
 export default (key = '') => {
   const logger = getLogger(`redis-${key}`);
 
   const client = new Redis(process.env.REDIS_URL, { no_ready_check: true });
-
-  // if (settings.username && settings.password) {
-  //   client.auth(settings.username, settings.password);
-  // }
 
   client.on('error', (err) => {
     logger.error(err.stack);
