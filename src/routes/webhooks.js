84d55644ae6b44487dc4e855f97127a1126d0163
Declan Ramsay <declan.ramsay@starlingbank.com>
2022-08-11 12:05:15 +0100

    Remove trailing bracket

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -78,7 +78,7 @@ export default function stripeWebhooksRoute() {
         },
         events: {
           title: `Event | ${document?.data?.event_name}`,
-          body: `${format(new Date(document?.data?.event_start_date), 'MMMM d, yyyy')} | ${document?.data?.venue}}`,
+          body: `${format(new Date(document?.data?.event_start_date), 'MMMM d, yyyy')} | ${document?.data?.venue}`,
           image: document?.data?.images?.[0]?.image?.url || null,
           data: { url: `/events/${document.uid}` },
         },
