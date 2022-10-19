b6b91150e51b4e93cf330e5b6ac2f6a955780d22
Declan Ramsay <declan.ramsay@starlingbank.com>
2022-10-19 22:03:53 +0100

    Add redis tls config item

--- src/modules/redis.js
+++ src/modules/redis.js
@@ -6,7 +6,12 @@ import getLogger from './logger';
 export default (key = '') => {
   const logger = getLogger(`redis-${key}`);
 
-  const client = new Redis(process.env.REDIS_URL, { no_ready_check: true });
+  const client = new Redis(process.env.REDIS_URL, {
+    no_ready_check: true,
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
   client.on('error', (err) => {
     logger.error(err.stack);
